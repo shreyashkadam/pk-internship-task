7c91f4ea0b472006c55ab7fa5027840287a68907
shreyashkadam <shreyash.kadam10@gmail.com>
2022-11-25 20:42:28 +0530

    Upload assignment 2

---
+++ pk-assignment2/src/Table/Table.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import { Card } from '@mui/material'
+import './Table.css'
+
+const Table = () => {
+
+    const [tableData, setTableData] = useState([])
+    let maleCount = 0;
+    let femaleCount = 0;
+
+    useEffect(() => {
+        fetch("https://gorest.co.in/public/v2/users")
+            .then((data) => data.json())
+            .then((data) => setTableData(data))
+    }, [])
+
+    for (let row of tableData) {
+        if (row.gender === 'male') {
+            maleCount++;
+        } else if (row.gender === 'female') {
+            femaleCount++;
+        }
+    }
+
+    const columns = [
+        { field: 'id', headerName: 'ID' },
+        { field: 'name', headerName: 'Name', width: 300 },
+        { field: 'email', headerName: 'E-Mail', width: 600 },
+        { field: 'gender', headerName: 'Gender', width: 300 },
+        { field: 'status', headerName: 'Status', width: 300 }
+    ]
+
+
+    return (
+        <div className='Table'>
+
+            <div style={{ height: 500, width: '100%' }}>
+                <DataGrid
+                    color="primary"
+                    rows={tableData}
+                    columns={columns}
+                    rowsPerPageOptions={[10]}
+                    pageSize={10}
+                />
+            </div>
+
+            <div className='counts'>
+                <div className='male-count'>
+                    Male : {maleCount}
+                </div>
+                <div className='female-count'>
+                    Female : {femaleCount}
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default Table
